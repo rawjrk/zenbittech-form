511981e7f3de98d8f1cc7ce7a7c4675012a0844d
RawJrk <rodjer7777@gmail.com>
2022-10-11 20:05:56 +0300

    back: replaced mysql package

--- back/services/db.js
+++ back/services/db.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config(); // eslint-disable-line global-require
 }
 
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 const dbCredentials = {
   host: process.env.MYSQL_HOST,
