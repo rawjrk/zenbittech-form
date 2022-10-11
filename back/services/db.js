ee65ce5dfbdd96df4020894a04a04136bbadd7bf
RawJrk <rodjer7777@gmail.com>
2022-10-11 23:41:32 +0300

    back: minor db conn fix

--- back/services/db.js
+++ back/services/db.js
@@ -6,6 +6,7 @@ const mysql = require("mysql2");
 
 const dbCredentials = {
   host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
