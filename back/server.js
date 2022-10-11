51eaf74d14ca3ed22f7137fe1c091c308040c9dd
RawJrk <rodjer7777@gmail.com>
2022-10-12 00:38:23 +0300

    back: port issue fix

--- back/server.js
+++ back/server.js
@@ -5,7 +5,7 @@ const AppError = require("./utils/AppError");
 const errorHandler = require("./utils/errorHandler");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
